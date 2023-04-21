eb64ee629cbbcddfe317a9ac4d822ea8b6d142dc
Amelia Donnelly <126174050+AmyDon12@users.noreply.github.com>
2023-04-21 14:45:25 +0100

    Added some 3D geometry calculating functions

--- amymath.h
+++ amymath.h
@@ -2,12 +2,9 @@
 #include <stdio.h>
 #include <cmath>
 
-//namespace mathlib {
-
-
  static int pythOPP(double Hypotenuse, double Adjacent) {  
 
-  //finds the opposite
+  // finds the opposite
   static double o  = sqrt(pow(Hypotenuse, 2) - pow(Adjacent, 2));
 
   //returns
@@ -19,20 +16,20 @@
 
   static int pythHYP(double Opposite, double Adjacent) {
 
-  //finds the opposite
+  // finds the opposite
   static double h  = sqrt(pow(Opposite, 2) + pow(Adjacent, 2));
 
-  //returns
+  // returns
   return h;
   
 }
 
   static int pythADJ(double Hypotenuse, double Opposite) {
 
-  //finds the opposite
+  // finds the opposite
   static double a  = sqrt(pow(Hypotenuse, 2) - pow(Opposite, 2));
 
-  //returns
+  // returns
   return a;
   // Remember: length of Hypotenuse can not be less than Opposite length 
   // or it will return -2.14748e+9
@@ -47,6 +44,7 @@ static int ml_L(double ml) {
 }
 
 static int l_ML(double l) {
+ 
     //  converts L to mL
   double output = l * 1000;
   return output; 
@@ -54,15 +52,112 @@ static int l_ML(double l) {
 }
 
 static int g_KG(double g) {
+ 
   // converts grams to kilograms
+
   double output = g / 1000;
   return output; 
   
 }
 
 static int kg_G(double kg) {
+ 
   // converts kilograms to grams
   double output = kg * 1000;
   return output; 
+  
+}
+
+static int sphVOL(double radius) {
+  
+  // finds the volume of a sphere using the radius
+  double output = ( (4/3) * M_PI * pow(radius, 3));
+  return output;
+  
+}
+
+static int sphTSA(double radius) {
+ 
+  // finds the total surface area of a sphere using its radius
+  double output = (4 * M_PI * pow(radius, 2));
+  return radius;
+  
+}
+
+static int cubeVOL(double edge) {
+  
+  // finds the volume of an equal sided cube, using the edge length
+  double output = pow(edge, 2);
+  return output; 
+  
+}
+
+static int cubeTSA(double edge) {
+  
+  // finds the total surface area of the cube using the edges 
+  double output = pow((6*edge), 2); 
+  return output;
+
+}
+
+static int cubdVOL(double length, double breadth, double height) {
+  
+ // finds the volume of a cuboid using the length, breadth and height
+  double output = (length * breadth * height);
+  return output;
+
+}
+
+static int cubdTSA(double length, double breadth, double height) {
+  
+  // finds the total surface area of a cuboid using the length breadth and height
+  double output = ((2*(length*breadth))+(2*(length*height))+ (2*(height*breadth)));
+  return output;
+
+}
+
+static int coneVOL(double radius, double height) {
+  
+  // finds the volume of a cone using the base radius and height
+  double output = (M_PI * (pow(radius, 2)) * (height/3));
+  return output;
+  
+}
+
+static int coneCSA(double radius, double height)  {
+  
+  // finds the curved surface area of a cone using the base radius and height
+  double output = ((M_PI * radius * height);
+  return output;
+  
+}
+static int coneTSA (double radius, double height) {
+
+  // finds the total surface area of a cone using the base radius and height
+  double output = (coneCSA(radius, height) + (2 * pow(radius, 2)));
+  return output;
+  
+}
+
+static int cylVOL(double radius, double height) {
+
+  // finds the volume of a cylinder by using the base radius and height
+  double output = (M_PI * pow(radius, 2) * height);
+  return output;
+  
+}
+static int cylCSA(double radius, double height) {
+  
+  // finds the curved surface area of a cylinder by using the base radius and height
+  double output = (2 * M_PI * radius * height);
+  return output;
+  
+}
+
+static int cylTSA(double radius, double height) {
+  
+  // finds the total surface area of a cylinder by using the base radius and height
+  double output = 2 * M_PI * radius * (height + radius);
+  return output;
   
 }
