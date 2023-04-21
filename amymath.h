b40b1000fbbdd106430379bfc20fbbcfb59a9775
Amelia Donnelly <126174050+AmyDon12@users.noreply.github.com>
2023-04-21 09:40:08 +0100

    Updated library header
    added conversion of units (mL~L, g~kg)

--- amymath.h
+++ amymath.h
@@ -14,7 +14,9 @@
   return o;
   // Remember: length of Hypotenuse can not be less than Adjacent length 
   // or it will return -2.14748e+9
+  
 }
+
   static int pythHYP(double Opposite, double Adjacent) {
 
   //finds the opposite
@@ -24,6 +26,7 @@
   return h;
   
 }
+
   static int pythADJ(double Hypotenuse, double Opposite) {
 
   //finds the opposite
@@ -33,5 +36,33 @@
   return a;
   // Remember: length of Hypotenuse can not be less than Opposite length 
   // or it will return -2.14748e+9
+   
 }
 
+static int ml_L(double ml) {
+  //  converts mL to L
+  double output = ml / 1000;
+  return output; 
+  
+}
+
+static int l_ML(double l) {
+    //  converts L to mL
+  double output = l * 1000;
+  return output; 
+  
+}
+
+static int g_KG(double g) {
+  // converts grams to kilograms
+  double output = g / 1000;
+  return output; 
+  
+}
+
+static int kg_G(double kg) {
+  // converts kilograms to grams
+  double output = kg * 1000;
+  return output; 
+  
+}
